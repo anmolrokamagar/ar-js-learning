18432b338455d4f3de87de58ca109c290cdd272d
anmolrokamagar <rokamagaranmol@gmail.com>
2025-11-24 09:22:55 +0545

    New update of hamburger menu with case in camel,pascal,snake,kebab,upper

--- hamburger-menu-using-HTML-CSS-JS/script.js
+++ hamburger-menu-using-HTML-CSS-JS/script.js
@@ -10,3 +10,41 @@ navEl.addEventListener('click', () => {
    navEl.classList.remove("nav-open");
     hamburgerEl.classList.remove("hamburger-open"); 
 })
+
+
+
+//1. camelCase
+
+// First word lowercase, next words capitalized.
+
+// Example: firstName, userEmail
+
+//2. PascalCase
+
+// Every word capitalized.
+
+// Example: FirstName, UserEmail
+
+// Often used for class names.
+
+//3. snake_case
+
+// Words separated by underscores, all lowercase.
+
+// Example: first_name, user_email
+
+//4. kebab-case
+
+// Words separated by hyphens, all lowercase.
+
+// Example: first-name, user-email
+
+// Often used in URLs or CSS classes.
+
+//5. UPPER_CASE
+
+// All letters capitalized, words separated by underscores.
+
+// Example: MAX_VALUE, API_KEY
+
+// Often used for constants.
