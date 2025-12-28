709dfdeb23c75bce6b153b655f5b7b98887b602d
anmolrokamagar <rokamagaranmol@gmail.com>
2025-12-28 15:35:49 +0545

    Update new js which is most important in Variation 4

---
+++ hamburger-menu-js/hamburger-menu-using-var-4-js/script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+// Optional: Close menu when a link is clicked
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    });
+});
